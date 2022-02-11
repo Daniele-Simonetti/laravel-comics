215239714f9056ea2ff738a378c9639cc35a0772
Daniele-Simonetti <daniele.simonetti99@outlook.it>
2022-02-11 16:28:26 +0100

    keep adding header structure

--- resources/views/guest/partials/header.blade.php
+++ resources/views/guest/partials/header.blade.php
@@ -1,22 +1,22 @@
-<header>
+<header class="container-70">
   <div class="logo">
     <img src="{{asset('img/dc-logo.png')}}" alt="logo">
-    <div class="header_nav">
-      <ul class="header_nav_list">
-        <li class="header_nav_list-item"><a href="">Characters</a></li>
-        <li class="header_nav_list-item"><a href="">Comics</a></li>
-        <li class="header_nav_list-item"><a href="">Movies</a></li>
-        <li class="header_nav_list-item"><a href="">Tv</a></li>
-        <li class="header_nav_list-item"><a href="">Games</a></li>
-        <li class="header_nav_list-item"><a href="">Collectibles</a></li>
-        <li class="header_nav_list-item"><a href="">Videos</a></li>
-        <li class="header_nav_list-item"><a href="">fans</a></li>
-        <li class="header_nav_list-item"><a href="">News</a></li>
-        <li class="header_nav_list-item"><a href="">Shop</a></li>
-      </ul>
-    </div>
-    <div class="header_input">
-      <input type="search" name="Search" id="Search" placeholder="Search">
-    </div>
+  </div>
+  <nav class="header_nav">
+    <ul class="header_nav_list">
+      <li class="header_nav_list-item"><a href="">Characters</a></li>
+      <li class="header_nav_list-item"><a href="">Comics</a></li>
+      <li class="header_nav_list-item"><a href="">Movies</a></li>
+      <li class="header_nav_list-item"><a href="">Tv</a></li>
+      <li class="header_nav_list-item"><a href="">Games</a></li>
+      <li class="header_nav_list-item"><a href="">Collectibles</a></li>
+      <li class="header_nav_list-item"><a href="">Videos</a></li>
+      <li class="header_nav_list-item"><a href="">fans</a></li>
+      <li class="header_nav_list-item"><a href="">News</a></li>
+      <li class="header_nav_list-item"><a href="">Shop</a></li>
+    </ul>
+  </nav>
+  <div class="header_input">
+    <input type="text" placeholder="Search">
   </div>
 </header>

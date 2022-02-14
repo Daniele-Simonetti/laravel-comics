18747a057b069ef1189f0dfe8505b744ef9ab5f8
Daniele-Simonetti <daniele.simonetti99@outlook.it>
2022-02-14 15:54:44 +0100

    keep adding structure

--- resources/views/guest/comic.blade.php
+++ resources/views/guest/comic.blade.php
@@ -7,18 +7,25 @@
 @section('content')
     <div class="comic__page">
         <img src="{{ $comic['thumb'] }}" alt="{{ $comic['series'] }}">
-        <h1>{{ $comic['title'] }}</h1>
-        <div class="comic__price">
-            <div class="price">
-                <h3>U.S. Price: <span>{{$comic['price']}}</span></h3>
-                <h3>Available</h3>
-            </div>
-            <div class="availability">
-                <h3>Check Availability</h3>
-            </div>
-        </div>
-        <div class="comic__text">
-            <p>{{ $comic['description'] }}</p>
-        </div>
+
+				<div class="box">
+					<div class="comic__text">
+							<h1>{{ $comic['title'] }}</h1>
+							<div class="comic__price">
+									<div class="price">
+											<h3>U.S. Price: <span class="color-white">{{$comic['price']}}</span></h3>
+											<h3 class="text-uppercase">Available</h3>
+									</div>
+									<div class="availability">
+											<h3 class="color-white">Check Availability</h3>
+									</div>
+							</div>
+							<p>{{ $comic['description'] }}</p>
+					</div>
+					<div class="comic__adv">
+						<h2 class="text-uppercase">Advertisement</h2>
+							<img src="{{ asset('img/adv.jpg') }}" alt="">
+					</div>
+				</div>
     </div>
 @endsection
